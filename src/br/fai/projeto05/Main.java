b8d28cc050fdb192a822db3158a148e07e95391a
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:51:06 -0300

    1o commit

---
+++ src/br/fai/projeto05/Main.java
@@ -0,0 +1,17 @@
+package br.fai.projeto05;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		
+		
+	}
+
+}
