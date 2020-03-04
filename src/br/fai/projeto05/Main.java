e2bfbf4bcc23a6ba828bcc438d03c799fd65c477
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:59:09 -0300

    3o commit

--- src/br/fai/projeto05/Main.java
+++ src/br/fai/projeto05/Main.java
@@ -13,6 +13,22 @@ public class Main {
 		
 		System.out.println("enviando esta branch para repositorio");
 		
+		for (int i = 0; i < 5; i++) {
+			
+			switch(i) {
+			case 1:
+				System.out.println("Primeira condicao");
+			break;
+			
+			case 2:
+				System.out.println("Segunda condicao");
+			break;
+			
+			case 3:
+				System.out.println("Não há condicao");
+			break;
+			}
+		}
 	}
 
 }
